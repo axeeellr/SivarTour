cc4142791d77973789ec70119563a3c950895502
axelcaballosinfreno69 <axelramireezz@gmail.com>
2023-07-10 20:37:19 -0600

    cambios

--- index.php
+++ index.php
@@ -66,11 +66,9 @@
         <button class="hero__button">COMENZAR &nbsp;<i class="fa-sharp fa-solid fa-arrow-right"></i></button>
         
         <form class="hero__search" id="hero__search" action="filtered.php">
-            <div class="search__container">
-                <input type="search" id="dondeInput" autofocus="autofocus" placeholder="Departamento" class="awesomplete" data-list="Ahuachapán, Cabañas, Chalatenango, Cuscatlán, Morazán, La Libertad, La Paz, La Unión, San Miguel, San Salvador, San Vicente, Santa Ana, Sonsonate, Usulután">
-                <input type="search" id="tipoInput" autofocus="autofocus" placeholder="Tipo" class="awesomplete" data-list="Playa, Campo, Cabañas, Parque, Bosque, Lago, Montaña, Ruinas">
-                <input type="submit" value="" class="red__button search__button">
-            </div>
+            <input type="search" id="dondeInput" autofocus="autofocus" placeholder="Departamento" class="awesomplete" data-list="Ahuachapán, Cabañas, Chalatenango, Cuscatlán, Morazán, La Libertad, La Paz, La Unión, San Miguel, San Salvador, San Vicente, Santa Ana, Sonsonate, Usulután">
+            <input type="search" id="tipoInput" autofocus="autofocus" placeholder="Tipo" class="awesomplete" data-list="Playa, Campo, Cabañas, Parque, Bosque, Lago, Montaña, Ruinas">
+            <input type="submit" value="" class="red__button search__button">
         </form>
     </div>
 
